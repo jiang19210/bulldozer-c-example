778c601c38aec14f04cb168461bda7fe540d5aa0
姜光明 <jiangguangming@u51.com>
2019-04-09 20:31:13 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # bulldozer-c-example
+
+# bulldozer-c
+
+#### 项目介绍
+主要组件:https://github.com/jiang19210/bulldozer-c
+服务端:https://github.com/jiang19210/bulldozer
+****
+#### 软件架构
+![avatar](https://github.com/jiang19210/data/blob/master/bulldozer.png?raw=true)
+****
+#### 安装教程
+1. 服务端安装：https://github.com/jiang19210/bulldozer
+2. 执行npm install
+
+#### 使用说明 mysql建表语句见sql.sql
+1. node dianping_one.js 
+2. node dianping_task.js 
---
+++ sql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `dianping_test_table`
+(
+  `AutoId`           bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `ShopId`               varchar(88)       DEFAULT NULL COMMENT '逻辑主键-店铺id',
+  `Url`              varchar(255)      DEFAULT NULL COMMENT '地址',
+  `CityName`         varchar(88)       DEFAULT NULL COMMENT '城市',
+  `ShopName`         varchar(500)      DEFAULT NULL COMMENT '店铺名称',
+  PRIMARY KEY (`AutoId`),
+  UNIQUE KEY `uq_Id` (`ShopId`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPRESSED COMMENT='大众点评测试'
